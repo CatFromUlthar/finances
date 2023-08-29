0595b1603edb621babcf333bdf139d71745c4c4d
arseniy <afomenko03@ya.ru>
2023-08-29 16:08:54 +0300

    added db interaction

--- main.py
+++ main.py
@@ -1,10 +1,19 @@
 import datetime
+from db_interactor import DataBaseInteractor
 
 
 class BudgetElement:
     obj_list = []
 
-    def __init__(self, amount: int | float, positivity: str, date: tuple = None):
+    def __init__(self, description: str, amount: int | float, positivity: str, date: tuple = None):
+
+        database_int = DataBaseInteractor('financial_spreadsheet.db')
+
+        database_int.create_table('fin_balance', description='TEXT', amount='INTEGER',
+                                  positivity='TEXT', date='TEXT')
+
+        self.description = description
+
         self.amount = amount
 
         if date is None:
@@ -18,6 +27,9 @@ class BudgetElement:
             raise ValueError("Only '+' (revenue) or '-' (costs) allowed")
 
         self.__class__.obj_list.append(self)
+
+        database_int.add_data('fin_balance', description=self.description, amount=self.amount,
+                              positivity=self.positivity, date=self.date.__str__())
 
     def __repr__(self):
         return f'BudgetElement(amount={self.amount}, positivity={self.positivity})'
@@ -37,8 +49,7 @@ class BudgetElement:
 
 
 if __name__ == '__main__':
-    water_bill = BudgetElement(500, '-', (1, 9, 2023))
-    paycheck = BudgetElement(1000, '+', (2, 9, 2023))
-    food = BudgetElement(150, '-')
+    water_bill = BudgetElement('water bill for last month', 500, '-', (1, 9, 2023))
+    paycheck = BudgetElement('my work paycheck', 1000, '+', (2, 9, 2023))
+    food = BudgetElement('food and groceries', 150, '-')
     BudgetElement.calculate_budget()
-    print(food.date.day)
