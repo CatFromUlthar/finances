b6579256ff1a6a1ee7d76d8d9ba58586c5af6459
arseniy <afomenko03@ya.ru>
2023-08-29 16:30:28 +0300

    minor changes

--- main.py
+++ main.py
@@ -5,12 +5,13 @@ from db_interactor import DataBaseInteractor
 class BudgetElement:
     obj_list = []
 
+    @classmethod
+    def create_spreadsheet(cls):
+        cls.database_int = DataBaseInteractor('financial_spreadsheet.db')
+        cls.database_int.create_table('fin_balance', description='TEXT', amount='INTEGER',
+                                                 positivity='TEXT', date='TEXT')
+
     def __init__(self, description: str, amount: int | float, positivity: str, date: tuple = None):
-
-        database_int = DataBaseInteractor('financial_spreadsheet.db')
-
-        database_int.create_table('fin_balance', description='TEXT', amount='INTEGER',
-                                  positivity='TEXT', date='TEXT')
 
         self.description = description
 
@@ -28,8 +29,9 @@ class BudgetElement:
 
         self.__class__.obj_list.append(self)
 
-        database_int.add_data('fin_balance', description=self.description, amount=self.amount,
-                              positivity=self.positivity, date=self.date.__str__())
+        self.__class__.database_int.add_data('fin_balance',
+                                             description=self.description, amount=self.amount,
+                                             positivity=self.positivity, date=self.date.__str__())
 
     def __repr__(self):
         return f'BudgetElement(amount={self.amount}, positivity={self.positivity})'
@@ -45,10 +47,11 @@ class BudgetElement:
             else:
                 budget -= i.amount
 
-        print(f'Общий бюджет составляет {budget}')
+        return budget
 
 
 if __name__ == '__main__':
+    BudgetElement.create_spreadsheet()
     water_bill = BudgetElement('water bill for last month', 500, '-', (1, 9, 2023))
     paycheck = BudgetElement('my work paycheck', 1000, '+', (2, 9, 2023))
     food = BudgetElement('food and groceries', 150, '-')
