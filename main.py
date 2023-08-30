a4958835e85175a69b4ee44ae530772e5573b0bb
arseniy <afomenko03@ya.ru>
2023-08-30 19:05:57 +0300

    added getting data from db

--- main.py
+++ main.py
@@ -4,14 +4,17 @@ from db_interactor import DataBaseInteractor
 
 class BudgetElement:
     obj_list = []
+    database_int = DataBaseInteractor('finances.db')
+    table_name = None
 
     @classmethod
-    def create_spreadsheet(cls):
-        cls.database_int = DataBaseInteractor('financial_spreadsheet.db')
-        cls.database_int.create_table('fin_balance', description='TEXT', amount='INTEGER',
+    def create_spreadsheet(cls, table_name: str):
+        cls.database_int.create_table(table_name, description='TEXT', amount='INTEGER',
                                       positivity='TEXT', date='TEXT')
+        cls.table_name = table_name
 
-    def __init__(self, description: str, amount: int | float, positivity: str, date: tuple = None):
+    def __init__(self, description: str, amount: int | float, positivity: str, date: tuple = None,
+                 add_to_db: bool = True):
 
         self.description = description
 
@@ -29,12 +32,29 @@ class BudgetElement:
 
         self.__class__.obj_list.append(self)
 
-        self.__class__.database_int.add_data('fin_balance',
-                                             description=self.description, amount=self.amount,
-                                             positivity=self.positivity, date=self.date.__str__())
+        if add_to_db is True:
+            self.__class__.database_int.add_data(self.__class__.table_name,
+                                                 description=self.description, amount=self.amount,
+                                                 positivity=self.positivity, date=self.date.__str__())
 
     def __repr__(self):
-        return f'BudgetElement(amount={self.amount}, positivity={self.positivity})'
+        return (f'BudgetElement(description={self.description}, amount={self.amount}, positivity={self.positivity}, '
+                f'date={self.date})')
+
+    @classmethod
+    def get_spreadsheet_data(cls, table_name: str):
+
+        cls.table_name = table_name
+        cls.obj_list = []
+
+        result = cls.database_int.get_data(table_name, '*')
+
+        for i in result:
+            description = i[0]
+            amount = i[1]
+            positivity = i[2]
+            date = (int(i[3][-2:]), int(i[3][5:7]), int(i[3][:4]))
+            x = BudgetElement(description, amount, positivity, date, add_to_db=False)
 
     @classmethod
     def _calculate_budget_base(cls, date_from: datetime.date, date_to: datetime.date):
@@ -76,9 +96,9 @@ class BudgetElement:
 
 
 if __name__ == '__main__':
-    BudgetElement.create_spreadsheet()
-    water_bill = BudgetElement('water bill for last month', 500, '-', (1, 9, 2023))
-    paycheck = BudgetElement('my work paycheck', 1000, '+', (2, 9, 2023))
-    food = BudgetElement('food and groceries', 150, '-', (20, 9, 2023))
-    fin_count = BudgetElement.calculate_budget(period_to=(10, 9, 2023))
-    print(fin_count)
+    # BudgetElement.create_spreadsheet('september')
+    # water_bill = BudgetElement('water bill for last month', 500, '-', (1, 8, 2023))
+    # paycheck = BudgetElement('my work paycheck', 1000, '+', (2, 8, 2023))
+    # food = BudgetElement('food and groceries', 150, '-', (20, 8, 2023))
+    BudgetElement.get_spreadsheet_data('september')
+    print(BudgetElement.calculate_budget())
